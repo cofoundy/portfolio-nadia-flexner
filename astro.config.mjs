5735c04f903e038ec88847b2045619ce963cd668
A-PachecoT <andre.pacheco.t@uni.pe>
2026-05-15 13:44:55 -0500

    sync: source code at deploy time

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cofoundy.github.io",
-  base: "/portfolio-nadia-flexner",
+  site: "https://nadiaflexner.cofoundy.dev",
   vite: {
     plugins: [tailwindcss()],
   },
